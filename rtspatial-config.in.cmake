9cb966b75b1776a441d03c299eb30a140c9ff54a
Liang Geng <geng.161@osu.edu>
2024-07-22 21:34:57 -0400

    Fix

--- rtspatial-config.in.cmake
+++ rtspatial-config.in.cmake
@@ -13,7 +13,37 @@ message("OptiX_INSTALL_DIR: ${OptiX_INSTALL_DIR}")
 
 include("${RTSPATIAL_LIBRARY_DIR}/cmake/rtspatial/FindOptiX.cmake")
 include("${RTSPATIAL_LIBRARY_DIR}/cmake/rtspatial/nvcuda_compile_module.cmake")
-include("${RTSPATIAL_LIBRARY_DIR}/cmake/rtspatial/rtspatial_compile_shaders.cmake")
+
+FUNCTION(RTSPATIAL_COMPILE_SHADERS CALLBACK_INCLUDE_DIR CALLBACK_HEADER OUTPUT_DIR GENERATED_FILES)
+    set(FLOAT_TYPES "float;double")
+    set(OPTIX_MODULE_EXTENSION ".ptx")
+    set(OPTIX_PROGRAM_TARGET "--ptx")
+
+    file(GLOB SHADERS "${RTSPATIAL_SHADER_DIR}/*.cu")
+
+    foreach (FLOAT_TYPE IN LISTS FLOAT_TYPES)
+        message("-- Defining shaders (FLOAT_TYPE: ${FLOAT_TYPE}, CALLBACK_INCLUDE_DIR: ${CALLBACK_INCLUDE_DIR}, CALLBACK_HEADER: ${CALLBACK_HEADER})")
+        NVCUDA_COMPILE_MODULE(
+                SOURCES ${SHADERS}
+                DEPENDENCIES ${SHADERS_HEADERS}
+                TARGET_PATH "${OUTPUT_DIR}"
+                PREFIX "${FLOAT_TYPE}_"
+                EXTENSION "${OPTIX_MODULE_EXTENSION}"
+                GENERATED_FILES PROGRAM_MODULES
+                NVCC_OPTIONS "${OPTIX_PROGRAM_TARGET}"
+                "--relocatable-device-code=true"
+                "--expt-relaxed-constexpr"
+                "-Wno-deprecated-gpu-targets"
+                "-I${OptiX_INCLUDE}"
+                "-I${RTSPATIAL_INCLUDE_DIR}"
+                "-I${CALLBACK_INCLUDE_DIR}"
+                "-DFLOAT_TYPE=${FLOAT_TYPE}"
+                -include "${CALLBACK_HEADER}"
+        )
+        list(APPEND ALL_GENERATED_FILES ${PROGRAM_MODULES})
+    endforeach ()
+    set(${GENERATED_FILES} ${ALL_GENERATED_FILES} PARENT_SCOPE)
+ENDFUNCTION()
 
 
 include_directories(${OptiX_INCLUDE})
